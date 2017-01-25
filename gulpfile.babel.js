babb91780eddf74812c7c053f92856da91b2011c
Luca Lischetti <luca@sirlisko.com>
2017-01-25 22:38:49 +0000

    commenting out images task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -43,10 +43,10 @@ gulp.task('copy', () =>
 
 gulp.task('images', () =>
 	gulp.src('public/images/**/*')
-		.pipe($.cache($.imagemin({
-			progressive: true,
-			interlaced: true
-		})))
+		// .pipe($.cache($.imagemin({
+		// 	progressive: true,
+		// 	interlaced: true
+		// })))
 		.pipe(gulp.dest('dist/images'))
 		.pipe($.size({title: 'images'}))
 );
